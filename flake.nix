34e991eaf022f8f1ee576801b19cff3de8289c48
Jaume Mateu <jaume.mateu.mateu@gmail.com>
2023-05-22 14:26:43 +0200

    use nix flakes

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Compotime";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        app = pkgs.poetry2nix.mkPoetryApplication {
+          python = pkgs.python311;
+          projectDir = ./.;
+          preferWheels = true;
+        };
+
+        packageName = "compotime";
+      in {
+        packages.${packageName} = app;
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ poetry ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
+}
