01229519315da97f4314b2861657b9b07706ec3f
Jaume Mateu <jaume.mateu.mateu@gmail.com>
2023-07-28 20:28:02 +0200

    integrate pre-commit to nix

--- flake.nix
+++ flake.nix
@@ -1,37 +1,69 @@
 {
   description = "Compotime";
-  
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.poetry2nix = {
     url = "github:nix-community/poetry2nix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, poetry2nix }:
-    let
-      systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      devShells = forAllSystems (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ poetry2nix.overlay ];
-          };
-
-          poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
-            projectDir = ./.;
-            editablePackageSources = {
-              compotime = ./compotime;
+  outputs = { self, nixpkgs, poetry2nix, flake-utils, pre-commit-hooks }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+          editablePackageSources = { compotime = ./compotime; };
+          python = pkgs.python311;
+          preferWheels = true;
+          groups = [ "dev" "docs" ];
+        };
+      in {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              lint-nix = {
+                enable = true;
+                name = "Run linters for flake.nix";
+                entry = "nixfmt flake.nix";
+                types = [ "nix" ];
+              };
+              lint-python = {
+                enable = true;
+                name = "Run linter for python files";
+                entry = "ruff compotime tests";
+                types = [ "python" ];
+              };
+              format = {
+                enable = true;
+                name = "Run formatters";
+                entry = "black . && ruff -s --fix --exit-zero .";
+                types = [ "python" ];
+              };
+              tests = {
+                enable = true;
+                name = "Run tests";
+                entry = "make tests";
+              };
+              docs = {
+                enable = true;
+                name = "Build documentation";
+                entry = let
+                  script = pkgs.writeShellScript "build_docs.sh" ''
+                    rm -rf docs/source/_autosummary docs/build/*
+                    make -C ./docs html
+                  '';
+                in toString script;
+              };
             };
-            python = pkgs.python311;
-            preferWheels = true;
-            groups = [ "dev" "docs" ];
           };
-        in
-        {
-          default = pkgs.mkShell { packages = [ poetryEnv pkgs.poetry pkgs.pandoc ]; };
-        });
-    };
+        };
+        devShell = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          packages = [ poetryEnv pkgs.poetry pkgs.pandoc pkgs.nixfmt ];
+        };
+      });
 }
